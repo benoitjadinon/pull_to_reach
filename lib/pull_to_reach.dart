9414fd84442bb71a764637f8d6bc1ce40aa7b38d
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2019-02-15 17:20:56 +0100

    library-release: expose widgets and index

--- lib/pull_to_reach.dart
+++ lib/pull_to_reach.dart
@@ -1,1 +1,6 @@
 library pull_to_reach;
+
+export 'package:pull_to_reach/index_calculator/weighted_index.dart';
+export 'package:pull_to_reach/pull_to_reach.dart';
+export 'package:pull_to_reach/widgets/reachable_icon.dart';
+export 'package:pull_to_reach/widgets/reachable_widget.dart';
